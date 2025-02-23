646c2591894b5f4291db2c582d9821b37ccc000a
azfasty <143274420+azfasty@users.noreply.github.com>
2025-02-23 22:49:27 +0100

    Update ouvrir_pages.py

--- ouvrir_pages.py
+++ ouvrir_pages.py
@@ -3,8 +3,8 @@ import time
 
 # Liste des URLs à ouvrir
 urls = [
-    "https://www.google.com",
-    "https://www.youtube.com"
+    "https://cdn.discordapp.com/attachments/1339570046338596966/1343259645585784903/IMG_1568.png?ex=67bc9f88&is=67bb4e08&hm=0948002f851ce1ebb74dfa8eda85d867700ce8f4be9b6c9d29ae48fc8ccb9b2f&",
+    "https://cdn.discordapp.com/attachments/1341471594761027659/1343338805603598458/IMG_1961.png?ex=67bce941&is=67bb97c1&hm=658528c1fc0b713d4c5fd699ba23fd57876647d0af42d18a7281a0b27123753d&"
 ]
 
 # Ouvrir chaque URL dans un nouvel onglet
